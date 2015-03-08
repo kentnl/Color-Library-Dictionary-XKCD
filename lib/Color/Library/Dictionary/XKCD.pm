b51374f01e6b3621e3d58d1b4ea30c51049ce3a5
Kent Fredric <kentfredric@gmail.com>
2015-03-09 01:42:45 +1300

    Move blob of POD to EOF so it doesnt create excess whitespace in the release

--- lib/Color/Library/Dictionary/XKCD.pm
+++ lib/Color/Library/Dictionary/XKCD.pm
@@ -9,15 +9,42 @@ our $VERSION = '1.000000';
 
 # AUTHORITY
 
+use Color::Library 0.021;
+use Moo;
+use File::ShareDir qw( dist_file );
+extends 'Color::Library::Dictionary';
+
+__PACKAGE__->_register_dictionary;
+
+sub _load_color_list {
+  my $file = dist_file( 'Color-Library-Dictionary-XKCD', 'color_list.pl' );
+  return [ do $file ];
+}
+
+sub _description {
+  return {
+    'subtitle'    => 'Colors determined by the XKCD Color Survey',
+    'title'       => 'XKCD',
+    'description' => <<"_EOF_",
+The web comic XKCD[http://xkcd.com] ran a rather wide-spread color survey
+to gauge the social perception of colors, at least, with regard to screen print spaces.
+
+The findings of this survey can be found here:
+
+[http://blog.xkcd.com/2010/05/03/color-survey-results/]
+_EOF_
+  };
+}
+no Moo;
+
+1;
+
 =head1 DESCRIPTION
 
 The web comic L<<< B<< C<XKCD> >>|http://xkcd.com >>> ran a rather wide-spread color survey
 to gauge the social perception of colors, at least, with regard to screen print spaces.
 
 The findings of this survey can be found here: L<<< B<< C<blog.xkcd.com/2010/05/03/color-survey-results/> >>|http://blog.xkcd.com/2010/05/03/color-survey-results/ >>>
-
-
-=cut
 
 =head1 COLORS
 
@@ -973,33 +1000,3 @@ The findings of this survey can be found here: L<<< B<< C<blog.xkcd.com/2010/05/
 
 
 =cut
-
-use Color::Library 0.021;
-use Moo;
-use File::ShareDir qw( dist_file );
-extends 'Color::Library::Dictionary';
-
-__PACKAGE__->_register_dictionary;
-
-sub _load_color_list {
-  my $file = dist_file( 'Color-Library-Dictionary-XKCD', 'color_list.pl' );
-  return [ do $file ];
-}
-
-sub _description {
-  return {
-    'subtitle'    => 'Colors determined by the XKCD Color Survey',
-    'title'       => 'XKCD',
-    'description' => <<"_EOF_",
-The web comic XKCD[http://xkcd.com] ran a rather wide-spread color survey
-to gauge the social perception of colors, at least, with regard to screen print spaces.
-
-The findings of this survey can be found here:
-
-[http://blog.xkcd.com/2010/05/03/color-survey-results/]
-_EOF_
-  };
-}
-no Moo;
-
-1;
