4d2efe819619d434d404a00d67eadb54b266705c
Kent Fredric <kentfredric@gmail.com>
2017-03-08 04:41:48 +1300

    Build results of 42cf5eb (on master)

--- lib/Color/Library/Dictionary/XKCD.pm
+++ lib/Color/Library/Dictionary/XKCD.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Color::Library::Dictionary::XKCD;
 
-our $VERSION = '1.000000';
+our $VERSION = '1.000001';
 
 # ABSTRACT: Colors determined by the XKCD Color Survey.
 
@@ -52,7 +52,7 @@ Color::Library::Dictionary::XKCD - Colors determined by the XKCD Color Survey.
 
 =head1 VERSION
 
-version 1.000000
+version 1.000001
 
 =head1 DESCRIPTION
 
@@ -1019,7 +1019,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
